464e222ef9316e1581b09a69d8c67f4fdc13bfe8
Jaime Aricapa <ja95aricapa@gmail.com>
2022-07-28 22:11:14 -0500

    task 1 v2

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,16 @@
-export default function getFullResponseFromAPI(success) {
+
+
+export default function getResponseFromAPI(success) {
   const thePromise = new Promise((resolve, reject) => {
     if (success === true) {
-      let answer = {'status': 200, 'body': 'Success'};
-      resolve(answer);
-    }
-    else {
-      let answer = 'The fake API is not working currently'
-      reject(answer);
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
     }
   });
+
   return thePromise;
 }
